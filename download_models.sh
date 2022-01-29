9a2f5e38663d2e98f5f16ce780f3fa9e1ddcdb29
brendo <you@example.com>
2022-01-29 10:32:57 +0800

    sheet parser

--- download_models.sh
+++ download_models.sh
@@ -36,10 +36,10 @@ fi
 if [ "$GUMBEL" = true ] ; then
   # vqgan_gumbel_f8_8192 (was openimages_f16_8192) - 376 MB:
   if [ ! -f "$LOC"/vqgan_gumbel_f8_8192.yaml ]; then
-    curl -L -o "$LOC"/vqgan_gumbel_f8_8192.yaml -C - 'https://heibox.uni-heidelberg.de/d/2e5662443a6b4307b470/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1' 
+    curl -L -o "$LOC"/vqgan_gumbel_f8_8192.yaml -C - 'https://heibox.uni-heidelberg.de/d/2e5662443a6b4307b470/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1'
   fi
   if [ ! -f "$LOC"/vqgan_gumbel_f8_8192.ckpt ]; then
-    curl -L -o "$LOC"/vqgan_gumbel_f8_8192.ckpt -C - 'https://heibox.uni-heidelberg.de/d/2e5662443a6b4307b470/files/?p=%2Fckpts%2Flast.ckpt&dl=1'    
+    curl -L -o "$LOC"/vqgan_gumbel_f8_8192.ckpt -C - 'https://heibox.uni-heidelberg.de/d/2e5662443a6b4307b470/files/?p=%2Fckpts%2Flast.ckpt&dl=1'
   fi
 fi
 
@@ -52,7 +52,7 @@ if [ "$COCO" = true ] ; then
     curl -L -o "$LOC"/coco.ckpt -C - 'https://dl.nmkd.de/ai/clip/coco/coco.ckpt' #COCO
   fi
 fi
-  
+
 if [ "$FACESHQ" = true ] ; then
 # faceshq:
   if [ ! -f "$LOC"/faceshq.yaml ]; then
@@ -64,8 +64,8 @@ if [ "$FACESHQ" = true ] ; then
 fi
 
 # Link?
-#if [ "$WIKIART_1024" = true ] ; then   
-#  # wikiart_1024 - 958 MB: 
+#if [ "$WIKIART_1024" = true ] ; then
+#  # wikiart_1024 - 958 MB:
 #  if [ ! -f "$LOC"/wikiart_1024.yaml ]; then
 #    curl -L -o "$LOC"/wikiart_1024.yaml -C - 'http://mirror.io.community/blob/vqgan/wikiart.yaml' #WikiArt 1024
 #  fi
@@ -75,7 +75,7 @@ fi
 #fi
 
 if [ "$WIKIART_16384" = true ] ; then
-  #wikiart_16384 - 1 GB: 
+  #wikiart_16384 - 1 GB:
   if [ ! -f "$LOC"/wikiart_16384.yaml ]; then
     curl -L -o "$LOC"/wikiart_16384.yaml -C - 'http://eaidata.bmk.sh/data/Wikiart_16384/wikiart_f16_16384_8145600.yaml' #WikiArt 16384
   fi
@@ -99,11 +99,11 @@ fi
 #ade20k:
 #  curl -L -o ade20k.yaml -C - 'https://static.miraheze.org/intercriaturaswiki/b/bf/Ade20k.txt' #ADE20K
 #  curl -L -o ade20k.ckpt -C - 'https://app.koofr.net/content/links/0f65c2cd-7102-4550-a2bd-07fd383aac9e/files/get/last.ckpt?path=%2F2020-11-20T21-45-44_ade20k_transformer%2Fcheckpoints%2Flast.ckpt' #ADE20K
-  
+
 #ffhq:
 #  curl -L -o ffhq.yaml -C - 'https://app.koofr.net/content/links/0fc005bf-3dca-4079-9d40-cdf38d42cd7a/files/get/2021-04-23T18-19-01-project.yaml?path=%2F2021-04-23T18-19-01_ffhq_transformer%2Fconfigs%2F2021-04-23T18-19-01-project.yaml&force' #FFHQ
 #  curl -L -o ffhq.ckpt -C - 'https://app.koofr.net/content/links/0fc005bf-3dca-4079-9d40-cdf38d42cd7a/files/get/last.ckpt?path=%2F2021-04-23T18-19-01_ffhq_transformer%2Fcheckpoints%2Flast.ckpt&force' #FFHQ
-  
+
 #celebahq:
 #  curl -L -o celebahq.yaml -C - 'https://app.koofr.net/content/links/6dddf083-40c8-470a-9360-a9dab2a94e96/files/get/2021-04-23T18-11-19-project.yaml?path=%2F2021-04-23T18-11-19_celebahq_transformer%2Fconfigs%2F2021-04-23T18-11-19-project.yaml&force' #CelebA-HQ
 #  curl -L -o celebahq.ckpt -C - 'https://app.koofr.net/content/links/6dddf083-40c8-470a-9360-a9dab2a94e96/files/get/last.ckpt?path=%2F2021-04-23T18-11-19_celebahq_transformer%2Fcheckpoints%2Flast.ckpt&force' #CelebA-HQ
