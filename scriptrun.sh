0c5bd1a5c9eb54e09a833137411d65fc2c7ee2e4
brendo <you@example.com>
2022-01-31 09:41:21 +0800

    script

--- scriptrun.sh
+++ scriptrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-while ! python makefromsheet.py -ss `pwd`/card_ai.ods -lr 0.4 -i 400 -s 600 590
+while ! python makefromsheet.py -ss card_ai.ods -lr 0.4 -i 400 -s 600 590
 do
-  sleep 1
+  sleep 15
   echo "Restarting program..."
 done
